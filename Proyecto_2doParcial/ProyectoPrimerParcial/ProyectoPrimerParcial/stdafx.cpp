6f682270e65150d9240d58d71d5e52583767a001
gabbyta95 <gabbyta95@users.noreply.github.com>
2017-12-27 13:51:02 -0500

    Menu + algunos extras
    Volvimos a subir el proyecto porque tuvimos problemas de compatibilidad  luego de la última actualización de visual studio.
    Creación del menú de login y menu principal. Incluimos la ayuda, pdf y qr, aunque aun no realizamos la llamada en el menu debido a que falta realizar el resto del programa.

---
+++ Proyecto_2doParcial/ProyectoPrimerParcial/ProyectoPrimerParcial/stdafx.cpp
@@ -0,0 +1,16 @@
+/************************************************
+*UNIVERSIDAD DE LAS FUERZAS ARMADAS "ESPE"      *
+*INTEGRANTES: GABRIELA ROSERO   EDWIN ASTUDILLO *
+*CARRERA: INGENIERIA EN SISTEMAS E INFORMÁTICA  *
+*INGENIERO: ING FERNANDO SOLIS                  *
+*Fecha de creación: 2017-11-11                  *
+*Fecha de modificación: 2017-11-27              *
+*************************************************/
+// stdafx.cpp : source file that includes just the standard includes
+// ConsoleApplication5.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
